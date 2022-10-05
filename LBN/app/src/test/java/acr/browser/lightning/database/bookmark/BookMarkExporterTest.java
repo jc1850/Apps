5c8c6eedc8375689739861270b92aacb98e3b6f6
propMiner <email@example.com>
2022-10-05 11:03:45 +0100

    fix test

--- LBN/app/src/test/java/acr/browser/lightning/database/bookmark/BookMarkExporterTest.java
+++ LBN/app/src/test/java/acr/browser/lightning/database/bookmark/BookMarkExporterTest.java
@@ -11,6 +11,7 @@ import android.app.Application;
 import android.util.Log;
 
 
+import androidx.annotation.NonNull;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.rule.GrantPermissionRule;
 
@@ -22,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.util.Scheduler;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -31,7 +33,13 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
+import acr.browser.lightning.browser.di.MainScheduler;
 import acr.browser.lightning.database.Bookmark;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableObserver;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 @RunWith(RobolectricTestRunner.class)
@@ -91,8 +99,30 @@ public class BookMarkExporterTest {
         File file  = BookmarkExporter.createNewExportFile();
         file.createNewFile();
         List<Bookmark.Entry> bookmarks = BookmarkExporter.importBookmarksFromAssets(ApplicationProvider.getApplicationContext());
-        BookmarkExporter.exportBookmarksToFile(bookmarks, file);
+        BookmarkExporter.exportBookmarksToFile(bookmarks, file).subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.computation())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        read[0] = true;
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        assert false;
+                    }
+
+
+                });
+        await().atMost(20,TimeUnit.SECONDS).until(() -> read[0]);
         bookmarks = BookmarkExporter.importBookmarksFromFileStream(new FileInputStream(file));
+
+        assert bookmarks.size() == 6;
         for (Bookmark.Entry item : bookmarks) {
             switch (item.getTitle()) {
                 case "Google":
